dfbcf94eb9183c873e6a50a100e16a0c76af9e75
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-08 21:38:58 +0000

    Add basic sidebar structure

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,14 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+     <aside className={`sidebar show-sidebar`}>
+       <div className="sidebar-header">
+         <img src={logo} className="logo" alt="Logo" />
+         <button className="close-btn"><FaTimes/></button>
+       </div>
+     </aside> 
+  )
 }
 
 export default Sidebar
