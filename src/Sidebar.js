d1ecebf36cc83bac49dfc4d4928b95d17d1ab988
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-09 08:05:32 +0000

    Add context api

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,8 +2,13 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import { useGlobalContext } from './context'
+
 
 const Sidebar = () => {
+
+  const {isSidebarOpen, closeSidebar} = useGlobalContext();
+
   return (
      <aside className={`sidebar`}>
        <div className="sidebar-header">
